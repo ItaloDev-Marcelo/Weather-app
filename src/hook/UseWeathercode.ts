cabd43340bdadda7963b12e276648fd5ab195cd5
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-08 10:36:25 -0300

    fix: fix image path on UseWeathercode

--- src/hook/UseWeathercode.ts
+++ src/hook/UseWeathercode.ts
@@ -1,12 +1,12 @@
 import type { WeatherIconsType } from "../types/Weather.Icons.type";
-import Drizzle from '../../assets/images/icon-drizzle.webp'
-import   Fog  from  '../../assets/images/icon-fog.webp'
-import  Overcast from '../../assets/images/icon-overcast.webp'
-import  PartlyCloudy from '../../assets/images/icon-partly-cloudy.webp'
-import Rain from '../../assets/images/icon-rain.webp'
-import Snow from  '../../assets/images/icon-snow.webp'
-import Storm from '../../assets/images/icon-storm.webp'
-import  Sunny from '../../assets/images/icon-sunny.webp'
+import Drizzle from '../assets/images/icon-drizzle.webp'
+import   Fog  from  '../assets/images/icon-fog.webp'
+import  Overcast from '../assets/images/icon-overcast.webp'
+import  PartlyCloudy from '../assets/images/icon-partly-cloudy.webp'
+import Rain from '../assets/images/icon-rain.webp'
+import Snow from  '../assets/images/icon-snow.webp'
+import Storm from '../assets/images/icon-storm.webp'
+import  Sunny from '../assets/images/icon-sunny.webp'
 
 export const WeatherIconsList:WeatherIconsType = {
         Drizzle,
